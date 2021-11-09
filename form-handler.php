58a899db71fd111008ab53e6e8064d3218c2b7e1
linhng22 <k.linh2210@gmail.com>
2021-11-08 20:27:38 -0700

    Whatever

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$email_from='info@yourwebsite.com';
+$email_from='nguyenk_7@sfu.ca';
 
 $email_subject='New Form Submission';
 
@@ -17,9 +17,9 @@ $to='k.linh2210@gmail.com';
 
 $headers ="From: $email_from \r\n";
 
-$headers ="Reply-To: $visitor_email \r\n";
+$headers .="Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body, $headers);
+mail($to, $email_subject, $email_body, $headers);
 
 header("Location: contact.html");
 
