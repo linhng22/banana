af984d95c6cc02d83463f8dc6c87bfa2d6a87800
linhng22 <k.linh2210@gmail.com>
2021-11-08 20:24:20 -0700

    Whatever

--- form-handler.php
+++ form-handler.php
@@ -17,7 +17,7 @@ $to='k.linh2210@gmail.com';
 
 $headers ="From: $email_from \r\n";
 
-$headers .="Reply-To: $visitor_email \r\n";
+$headers ="Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body, $headers);
 
